d858834b25098a65a72ecaabef9b08d769cfd47a
zaidsidd360 <zaidsidd360@gmail.com>
2023-10-25 04:11:27 +0530

    Done with the basic structure of the terminal

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import { Terminal } from "../../src/index";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-container">
+      <Terminal />
+    </div>
+  );
+}
+
+export default App;
